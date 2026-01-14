f8d3da7924a406c98152ba249335d65b26e175be
Marcos Vinicius Reis <m.vinicius.sr11@gmail.com>
2026-01-14 02:41:57 -0300

    criação do .env com secrets para deploy

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://zpnwfgkyswexznhkkjpw.supabase.co";
-const supabaseAnonKey =
-  "sb_publishable_HgJ1EwxuEIMarOX1TVlAFQ_Vgsmb_ai";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
